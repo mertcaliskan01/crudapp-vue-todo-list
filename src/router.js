ea089116023fe222ddc947bb8971cc0f14f2d564
Mert <mcaliskanmert>
2022-11-06 22:52:42 +0300

    Dynamic Page Title

--- src/router.js
+++ src/router.js
@@ -13,48 +13,78 @@ const routes = [
     path: "/",
     name: "home",
     component: Home,
+    meta: {
+      title:"Home"
+    },
   },
   {
     path: "/home",
     component: Home,
+    meta: {
+      title:"Home"
+    },
   },
   {
     path: "/login",
     component: Login,
+    meta: {
+      title:"Sign In"
+    },
   },
   {
     path: "/register",
     component: Register,
+    meta: {
+      title:"Sign Up"
+    },
   },
   {
     path: "/profile",
     name: "profile",
+    meta: {
+      title:"Profile"
+    },
     // lazy-loaded
     component: Profile,
   },
   {
     path: "/tasks",
     name: "tasks",
+    meta: {
+      title:"Todo List"
+    },
     // lazy-loaded
     component: TaskList,
   },
   {
     path: "/newTask",
     name: "newTask",
+    meta: {
+      title:"New Todo"
+    },
     // lazy-loaded
     component: TaskAdd,
   },
   {
     path: "/editTask/:id",
     name: "editTask",
+    meta: {
+      title:"Edit Todo"
+    },
     // lazy-loaded
     component: TaskEdit,
   },
-];
+]; 
 
 const router = createRouter({
   history: createWebHistory(),
   routes,
+});
+
+/* Page Title Changing*/
+router.beforeEach((to,from,next) => {
+  document.title = `${to.meta.title}`;
+  next();
 });
 
 router.beforeEach((to, from, next) => {
