b898f7b2734c8c1abfb1d9fa5f9ee16860e5e6dd
Mert <mcaliskanmert>
2022-11-07 17:14:03 +0300

    changed api url for deploy

--- src/services/http-common.js
+++ src/services/http-common.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export default axios.create({
-  baseURL: "http://localhost:8080/",
+  baseURL: "https://node-jwt-todo-api.herokuapp.com/",
   headers: {
     "Content-Type": "application/json"
   }
