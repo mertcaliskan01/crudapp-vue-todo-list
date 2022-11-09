7c1f043a53222641bb07ca8a6782b50e011f0c42
Mert <mcaliskanmert>
2022-11-09 21:22:02 +0300

    Navbar edited

--- src/App.vue
+++ src/App.vue
@@ -1,48 +1,68 @@
 <template>
   <div id="app">
-    <nav class="navbar navbar-expand navbar-dark bg-dark">
-      <a href="/" class="navbar-brand">Home</a>
-      <div class="navbar-nav mr-auto">
-        <li v-if="showTaskList" class="nav-item">
-          <router-link to="/tasks" class="nav-link"
-            ><font-awesome-icon icon="tasks" /> TodoList</router-link
-          >
-        </li>
-      </div>
+    <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+      <button
+        class="navbar-toggler"
+        type="button"
+        data-toggle="collapse"
+        data-target="#navbarSupportedContent"
+        aria-controls="navbarSupportedContent"
+        aria-expanded="false"
+        aria-label="Toggle navigation"
+      >
+        <span class="navbar-toggler-icon"></span>
+      </button>
 
-      <div v-if="!currentUser" class="navbar-nav ml-auto">
-        <li class="nav-item">
-          <router-link to="/register" class="nav-link">
-            <font-awesome-icon icon="user-plus" /> Sign Up
-          </router-link>
-        </li>
-        <li class="nav-item">
-          <router-link to="/login" class="nav-link">
-            <font-awesome-icon icon="sign-in-alt" /> Login
-          </router-link>
-        </li>
-      </div>
+      <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul class="navbar-nav mr-auto">
+          <li class="nav-item">
+            <router-link to="/home" class="nav-link">
+              <font-awesome-icon icon="home" /> Home
+            </router-link>
+          </li>
 
-      <div v-if="currentUser" class="navbar-nav ml-auto">
-        <li class="nav-item">
-          <router-link to="/profile" class="nav-link">
-            <font-awesome-icon icon="user" />
-            {{ currentUser.first_name }}
-          </router-link>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" @click.prevent="logOut">
-            <font-awesome-icon icon="sign-out-alt" /> LogOut
-          </a>
-        </li>
+          <li v-if="showTaskList" class="nav-item">
+            <router-link to="/tasks" class="nav-link"
+              ><font-awesome-icon icon="tasks" /> TodoList</router-link
+            >
+          </li>
+        </ul>
+
+        <div class="form-inline my-2 my-lg-0">
+          <div v-if="!currentUser" class="navbar-nav ml-auto">
+            <li class="nav-item">
+              <router-link to="/register" class="nav-link">
+                <font-awesome-icon icon="user-plus" /> Sign Up
+              </router-link>
+            </li>
+            <li class="nav-item">
+              <router-link to="/login" class="nav-link">
+                <font-awesome-icon icon="sign-in-alt" /> Login
+              </router-link>
+            </li>
+          </div>
+
+          <div v-if="currentUser" class="navbar-nav ml-auto">
+            <li class="nav-item">
+              <router-link to="/profile" class="nav-link">
+                <font-awesome-icon icon="user" />
+                {{ currentUser.first_name }}
+              </router-link>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" @click.prevent="logOut">
+                <font-awesome-icon icon="sign-out-alt" /> LogOut
+              </a>
+            </li>
+          </div>
+        </div>
       </div>
     </nav>
 
-    <div class="vh-100 gradient-custom" >
+    <div class="vh-100 gradient-custom">
       <router-view />
       <footer-section />
-    </div >
-
+    </div>
   </div>
 </template>
 
