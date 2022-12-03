acb16b426e4f131ed2a3053bb42f69c336f8019e
Mert <mcaliskanmert>
2022-12-03 11:00:42 +0300

    sass installed, main.scss created

--- src/main.js
+++ src/main.js
@@ -2,10 +2,8 @@ import { createApp } from "vue";
 import App from "./App.vue";
 import router from "./router";
 import store from "./store";
-import "bootstrap";
-import "bootstrap/dist/css/bootstrap.min.css";
 import { FontAwesomeIcon } from './plugins/font-awesome'
-import '../assets/css/main.css'
+import '../assets/css/main.scss'
 
 createApp(App)
   .use(router)
