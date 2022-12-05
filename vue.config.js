e4582b84b916354714b90a4fefbdadca787c0014
Mert <mcaliskanmert>
2022-12-06 00:59:10 +0300

    publish

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,8 @@
+const path = require("path");
+
 module.exports = {
-    devServer: {
-      port: 8081
-    }
+  outputDir: path.resolve(__dirname, "../public"),
+  devServer: {
+    port: 8081
   }
+}
